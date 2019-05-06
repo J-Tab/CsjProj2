a841be62285b78e27f294aa68edbd582e0c92fa6
Jeremy Tabujara <jemy_T20@hotmail.com>
2019-05-06 16:18:47 -0700

    Database added to git

--- src/main/java/com/csj/CsjProj2/CsjProj2Application.java
+++ src/main/java/com/csj/CsjProj2/CsjProj2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
+
 @SpringBootApplication
 @EnableOAuth2Sso
 public class CsjProj2Application {
